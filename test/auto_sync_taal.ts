36ac1d9a504c0e5c28e38298d9d40f3a25db156d
ccoincash <ccoincash@gmail.com>
2021-12-08 09:08:04 +0800

    all

--- test/auto_sync_taal.ts
+++ test/auto_sync_taal.ts
@@ -1,7 +1,7 @@
 import { MAPI_TARGET } from "bsv-util";
 import { MempoolSyncer } from "../src/mempoolAutoSyncer";
 
-let taalSyncer = new MempoolSyncer(MAPI_TARGET.GORILLA);
+let taalSyncer = new MempoolSyncer(MAPI_TARGET.TAAL);
 taalSyncer.restartJob();
 
 process.on("uncaughtException", (e) => {
